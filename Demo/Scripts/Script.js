181ae4e53e842ecd821538e0980f83112d38feeb
Randeep Singh <ransingh@ciena.com>
2017-08-08 21:01:00 +0530

    sort on header click, css etc.

--- Demo/Scripts/Script.js
+++ Demo/Scripts/Script.js
@@ -13,5 +13,18 @@
 
         $scope.employees = employees;
         $scope.rowLimit = 3;
-        $scope.sortOrder = "name";
+        $scope.sortColumn = "name";
+        $scope.reverseSort = false;
+
+        $scope.sortData = function (column) {
+            $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
+            $scope.sortColumn = column;
+        }
+
+        $scope.getSortClass = function (column) {
+            if ($scope.sortColumn == column) {
+                return $scope.reverseSort ? 'arrow-down' : 'arrow-up'
+            }
+            return '';
+        }
     });
